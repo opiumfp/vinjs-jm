4e483f52d43777d5932c95b084cf3442aa8474fd
Aleksandr Pavlovsky <opiumfp@gmail.com>
2021-02-15 02:31:03 +0200

    Fiexd HeroPT and ContactUs

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -11,9 +11,9 @@
             <div class="contact-us_social-icons navbar-expand d-inline-block h4 my-3">
               <social-icons :socialIconsData="this.socialData.socialicons"></social-icons>
             </div>
-            <div class="btn-group mb-3">
-              <a class="btn btn-secondary py-0" :href="/policy/">Policy</a></a>
-              <a class="btn btn-secondary py-0" href="http://vinnytsiajs.org/docs/Code_of_Conduct_-_VinnytsiaJS.pdf">Code of Conduct</a></a>
+            <div class="mb-3">
+              <a class="btn btn-secondary py-1 m-1" :href="/policy/">Policy</a></a>
+              <a class="btn btn-secondary py-1 m-1" href="http://vinnytsiajs.org/docs/Code_of_Conduct_-_VinnytsiaJS.pdf">Code of Conduct</a></a>
             </div>
             <ul class="list-unstyled">
               <li>
