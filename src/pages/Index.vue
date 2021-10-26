4aed27cc9ba58068f94530d553cc9cc39a02c5aa
Oleksandr Pavlovskyi <opiumfp@gmail.com>
2021-10-26 13:40:25 +0300

    Updated Telegram Widget

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -197,7 +197,7 @@
       <div class="spacer-nav-bottom"></div>
     </div>
 
-    <telegram-widget channel="vinjs" postid="114"></telegram-widget>
+    <telegram-widget channel="vinjs" postid="141"></telegram-widget>
 
   </Layout>
 </template>
