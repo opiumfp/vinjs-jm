8466a59b6aef1019b30e6f8438d15c0d8d6aa1c8
Oleksandr Pavlovskyi <opiumfp@gmail.com>
2022-01-02 23:25:53 +0200

    Updated bg section colors

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -23,7 +23,7 @@
         ></hero-pt>
       </section> -->
       <section
-        class="py-7 vjs-bg-logo vjs-bg-logo-blue text-white section-vh-100 d-flex align-items-center justify-content-center"
+        class="py-7 vjs-bg-logo vjs-bg-logo-red text-white section-vh-100 d-flex align-items-center justify-content-center"
         :id="$page.pageData.about.id"
       >
         <about v-if="$page.pageData.about.title" :aboutData="$page.pageData.about"></about>
@@ -33,7 +33,7 @@
         <stat v-if="$page.pageData.stat.items" :statData="$page.pageData.stat"></stat>
       </section>
      
-      <section :id="$page.pageData.mediaTiles.id" class="vjs-bg-logo vjs-bg-logo-red text-white">
+      <section :id="$page.pageData.mediaTiles.id" class="vjs-bg-logo vjs-bg-logo-green text-white">
         <div class="container">
           <div class="row">
             <div class="col-12">
