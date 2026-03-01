4848740a85123db9aee0cb881b94b66498c38063
Oleksandr Pavlovskyi <oleksandr_pavlovskyi@epam.com>
2026-03-01 02:00:46 +0200

    Next.js: Final migration

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89992e1ee20edec3"; }
+/******/ 		__webpack_require__.h = function() { return "fe2e500141989e1c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
