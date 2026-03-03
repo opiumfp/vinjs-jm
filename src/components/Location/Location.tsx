e344c3560137e15cd1dba6d0e9b1ec3f1197dfb0
Oleksandr Pavlovskyi <oleksandr_pavlovskyi@epam.com>
2026-03-03 18:08:18 +0200

    fix(Location): replace img with next/image for improved performance and responsiveness

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,4 +1,5 @@
 import type { LocationData } from '@/types/content';
+import Image from 'next/image';
 
 interface Props {
   locationData: LocationData;
@@ -35,20 +36,26 @@ export default function Location({ locationData }: Props) {
           <div className="location_bg">
             <div className="location_bg_wrap">
               <div className="location_bg_prlx">
-                <img
+                <Image
                   className="location_bg_img location_bg_img-landscape"
                   src={`${basePath}${locationData.image}`}
                   alt="VinnytsiaJS - Butterfly"
                   loading="lazy"
                   decoding="async"
+                  objectFit='cover'
+                  fill
+                  sizes="100vw"
                 />
                 {locationData.imagePt && (
-                  <img
+                  <Image
                     className="location_bg_img location_bg_img-portrait"
                     src={`${basePath}${locationData.imagePt}`}
                     alt="VinnytsiaJS - Butterfly"
                     loading="lazy"
                     decoding="async"
+                    objectFit='cover'
+                    fill
+                    sizes="100vw"
                   />
                 )}
               </div>
