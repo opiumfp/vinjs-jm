d7cdf58acb5de3638d2f8d28134694f5821ce14a
Oleksandr Pavlovskyi <oleksandr_pavlovskyi@epam.com>
2026-03-03 23:54:44 +0200

    feat(App): add favicon links to Head component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../../assets/styles/main.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
